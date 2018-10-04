9a5cf1f6cfa472e6e21cadc6882c55bd7f31450e
Shawn Feiz <shawnfeiz@gmail.com>
2018-10-03 21:04:41 -0500

    Eh it's been awhile

---
+++ app.js
@@ -0,0 +1,27 @@
+$(".headshot").on("click", function(e){
+    if(e.currentTarget.alt === "Brett") {
+        $(".showerPic").empty();
+        var pic = $("<img>");
+        pic.attr("src", "Brett.png");
+        pic.addClass("showerPic mainPic");
+        $(".showerPic").append(pic);
+    }
+    else if (e.currentTarget.alt === "Daniel") {
+        $(".showerPic").empty();
+        var pic = $("<img>");
+        pic.attr("src", "Daniel.png");
+        pic.addClass("showerPic mainPic");
+        $(".showerPic").append(pic);
+    }
+    else if(e.currentTarget.alt === "Shawn") {
+        $(".showerPic").empty();
+        var pic = $("<img>");
+        pic.attr("src", "Shawn.png");
+        pic.addClass("showerPic mainPic");
+        $(".showerPic").append(pic);
+    }
+});
+
+$(".showerPic").on("click", function(){
+    $(".mainPic").remove();
+})
